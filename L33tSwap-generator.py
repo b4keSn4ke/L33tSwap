94f9dc784b56a9038af9e411b25d5d1c59431da2
Jean Goulet <jeanjrgoulet@gmail.com>
2024-10-14 13:23:50 -0400

    Repository creation

---
+++ L33tSwap-generator.py
@@ -0,0 +1,162 @@
+# Date: 2024-10-14
+# Name: L33tSwap-generator.py
+# Description: L33tSwap Rules set generation script
+# Author: b4keSn4ke
+# Github: https://github.com/b4keSn4ke
+# Project Repo: https://github.com/b4keSn4ke/L33tSwap
+# Version: 1.0
+# 
+#!/usr/bin/python3 
+
+# Lowercase and uppercase letters
+# Uncomment the array below if you want to permutate lowercase and uppercase letters - comment the array below if using another sets of character.
+#char = ['sa@','sa4','si1','si!','se#','se3','se£','se€','suv','so0','ss$','ss5','sA@','sA4','sI1','sI!','sE#','sE3','sE£','sE€','sUV','sO0','sS$','sS5']
+
+# Only lowercase letters - comment the array below if using another sets of character.
+char = ['sa@','sa4','si1','si!','se#','se3','se£','se€','suv','so0','ss$','ss5']
+
+# Only uppercase letters - comment the array below if using another sets of character.
+#char = ['sA@','sA4','sI1','sI!','sE#','sE3','sE£','sE€','sUV','sO0','sS$','sS5']
+
+
+print ("# This set of rules will try to replace vowels from a word \n# for special characters or numbers that could be interpreted like the vowels")
+print ("\n# It will also append up to 2 digits from 0 to 9 at the end of the word.\n")
+print ("\n# Swap 1 vowel in a word\n# for a special character looking similar\n")
+
+for c in char:
+    print(c)
+
+print ("\n# Swap 2 vowels in a word\n# for a special character looking similar\n")
+# Permutate vowels only
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        if char[i][1] != char[j][1]:
+            print (char[i] + " " + char[j])
+
+
+print ("\n# Swap 3 vowels in a word\n# for a special character looking similar\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for k in range (j+1, len(char)):
+            if (char[i][1] != char[j][1]) and (char[j][1] != char[k][1]):
+                print (char[i] + " " + char[j] + " " + char[k])
+
+print ("\n# Swap 4 vowels in a word\n# for a special character looking similar\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for k in range (j+1, len(char)):
+            for l in range (k+1, len(char)):
+                if (char[i][1] != char[j][1]) and (char[j][1] != char[k][1]) and (char[k][1] != char[l][1]):
+                    print (char[i] + " " + char[j] + " " + char[k] + " " + char[l])
+
+print ("\n# Swap all vowels in a word\n# for a special character looking similar\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for k in range (j+1, len(char)):
+            for l in range (k+1, len(char)):
+                for m in range (l+1, len(char)):
+                    if (char[i][1] != char[j][1]) and (char[j][1] != char[k][1]) and (char[k][1] != char[l][1]) and (char[l][1] != char[m][1]):
+                        print (char[i] + " " + char[j] + " " + char[k] + " " + char[l] + " " + char[m])
+
+# Permutate vowels and add a single digit (0-9)
+
+print ("\n# Swap 1 vowel in a word\n# for a special character looking similar\n# Appending 1 digit at the end of the word (0-9)\n")
+
+for c in char:   
+    for x in range (10):
+        print(c + " $" + str(x))
+
+
+print ("\n# Swap 2 vowels in a word\n# for a special character looking similar\n# Appending 1 digit at the end of the word (0-9)\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for x in range (10):
+            if char[i][1] != char[j][1]:
+                print (char[i] + " " + char[j] + " $" + str(x))
+
+print ("\n# Swap 3 vowels in a word\n# for a special character looking similar\n# Appending 1 digit at the end of the word (0-9)\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for k in range (j+1, len(char)):
+            for x in range (10):
+                if (char[i][1] != char[j][1]) and (char[j][1] != char[k][1]):
+                    print (char[i] + " " + char[j] + " " + char[k]+ " $" + str(x))
+
+print ("\n# Swap 4 vowels in a word\n# for a special character looking similar\n# Appending 1 digit at the end of the word (0-9)\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for k in range (j+1, len(char)):
+            for l in range (k+1, len(char)):
+                for x in range (10):
+                    if (char[i][1] != char[j][1]) and (char[j][1] != char[k][1]) and (char[k][1] != char[l][1]):
+                        print (char[i] + " " + char[j] + " " + char[k] + " " + char[l]+ " $" + str(x))
+
+print ("\n# Swap all vowels in a word\n# for a special character looking similar\n# Appending 1 digit at the end of the word (0-9)\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for k in range (j+1, len(char)):
+            for l in range (k+1, len(char)):
+                for m in range (l+1, len(char)):
+                    for x in range (10):
+                        if (char[i][1] != char[j][1]) and (char[j][1] != char[k][1]) and (char[k][1] != char[l][1]) and (char[l][1] != char[m][1]):
+                            print (char[i] + " " + char[j] + " " + char[k] + " " + char[l] + " " + char[m]+ " $" + str(x))
+
+
+# Permutate vowels and add two digits ( 0-9)
+print ("\n# Swap 1 vowel in a word\n# for a special character looking similar\n# Appending 2 digit at the end of the word (0-9)\n")
+
+for c in char:   
+    for x in range (10):
+        for y in range (10):
+            print(c + " $" + str(x) + " $" + str(y))
+
+
+print ("\n# Swap 2 vowels in a word\n# for a special character looking similar\n# Appending 2 digit at the end of the word (0-9)\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for x in range (10):
+            for y in range (10):
+                if char[i][1] != char[j][1]:
+                    print (char[i] + " " + char[j] + " $" + str(x) + " $" + str(y))
+
+print ("\n# Swap 3 vowels in a word\n# for a special character looking similar\n# Appending 2 digit at the end of the word (0-9)\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for k in range (j+1, len(char)):
+            for x in range (10):
+                for y in range (10):
+                    if (char[i][1] != char[j][1]) and (char[j][1] != char[k][1]):
+                        print (char[i] + " " + char[j] + " " + char[k]+ " $" + str(x)+ " $" + str(y))
+
+print ("\n# Swap 4 vowels in a word\n# for a special character looking similar\n# Appending 2 digit at the end of the word (0-9)\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for k in range (j+1, len(char)):
+            for l in range (k+1, len(char)):
+                for x in range (10):
+                    for y in range (10):
+                        if (char[i][1] != char[j][1]) and (char[j][1] != char[k][1]) and (char[k][1] != char[l][1]):
+                            print (char[i] + " " + char[j] + " " + char[k] + " " + char[l]+ " $" + str(x)+ " $" + str(y))
+
+print ("\n# Swap all vowels in a word\n# for a special character looking similar\n# Appending 2 digit at the end of the word (0-9)\n")
+
+for i in range(len(char)):
+    for j in range (i+1, len(char)):
+        for k in range (j+1, len(char)):
+            for l in range (k+1, len(char)):
+                for m in range (l+1, len(char)):
+                    for x in range (10):
+                        for y in range (10):
+                            if (char[i][1] != char[j][1]) and (char[j][1] != char[k][1]) and (char[k][1] != char[l][1]) and (char[l][1] != char[m][1]):
+                                print(char[i] + " " + char[j] + " " + char[k] + " " + char[l] + " " + char[m]+ " $" + str(x)+ " $" + str(y))
--- README.md
+++ README.md
@@ -1,2 +1,231 @@
 # L33tSwap
-Set of cracking rule for Hashcat that swap vowels from words in L33t format
+Set of cracking rule for Hashcat that swap vowels from words in L33t format.
+
+You can customize the list by commenting/uncommenting the sets of characters you want to use to generate the rules list. 
+
+By default only `lowercase letters` are generated since it can get quite big.
+
+```
+#!/usr/bin/python3 
+
+# Lowercase and uppercase letters
+# Uncomment the array below if you want to permutate lowercase and uppercase letters - comment the array below if using another sets of character.
+#char = ['sa@','sa4','si1','si!','se#','se3','se£','se€','suv','so0','ss$','ss5','sA@','sA4','sI1','sI!','sE#','sE3','sE£','sE€','sUV','sO0','sS$','sS5']
+
+# Only lowercase letters - comment the array below if using another sets of character.
+char = ['sa@','sa4','si1','si!','se#','se3','se£','se€','suv','so0','ss$','ss5']
+
+# Only uppercase letters - comment the array below if using another sets of character.
+#char = ['sA@','sA4','sI1','sI!','sE#','sE3','sE£','sE€','sUV','sO0','sS$','sS5']
+```
+
+After having selected your sets of characters you can simply generate the list by running the following command
+
+```
+python3 L33tSwap-generator.py > L33tSwap.rule
+```
+
+
+Here are examples of permuttation that can be achieved using this sets of rule (`Lowercase and uppercase letters`) with the input word `Sequoia`
+
+```
+Sequoia -> 5equoia
+Sequoia -> $equoia
+Sequoia -> 53quoia
+Sequoia -> $3quoia
+Sequoia -> 5#quoia
+Sequoia -> $#quoia
+Sequoia -> 5£quoia
+Sequoia -> $£quoia
+Sequoia -> 5€quoia
+Sequoia -> $€quoia
+Sequoia -> $3qvoia
+Sequoia -> 5#qvoia
+Sequoia -> $#qvoia
+Sequoia -> 5£qvoia
+Sequoia -> $£qvoia
+Sequoia -> 5€qvoia
+Sequoia -> $€qvoia
+Sequoia -> $3qu0ia
+Sequoia -> 5#qu0ia
+Sequoia -> $#qu0ia
+Sequoia -> 5£qu0ia
+Sequoia -> $£qu0ia
+Sequoia -> 5€qu0ia
+Sequoia -> $€qu0ia
+Sequoia -> $3qv0ia
+Sequoia -> 5#qv0ia
+Sequoia -> $#qv0ia
+Sequoia -> 5£qv0ia
+Sequoia -> $£qv0ia
+Sequoia -> 5€qv0ia
+Sequoia -> $€qv0ia
+Sequoia -> $3quo1a
+Sequoia -> 5#quo1a
+Sequoia -> $#quo1a
+Sequoia -> 5£quo1a
+Sequoia -> $£quo1a
+Sequoia -> 5€quo1a
+Sequoia -> $€quo1a
+Sequoia -> $3qvo1a
+Sequoia -> 5#qvo1a
+Sequoia -> $#qvo1a
+Sequoia -> 5£qvo1a
+Sequoia -> $£qvo1a
+Sequoia -> 5€qvo1a
+Sequoia -> $€qvo1a
+Sequoia -> $3qu01a
+Sequoia -> 5#qu01a
+Sequoia -> $#qu01a
+Sequoia -> 5£qu01a
+Sequoia -> $£qu01a
+Sequoia -> 5€qu01a
+Sequoia -> $€qu01a
+Sequoia -> $3qv01a
+Sequoia -> 5#qv01a
+Sequoia -> $#qv01a
+Sequoia -> 5£qv01a
+Sequoia -> $£qv01a
+Sequoia -> 5€qv01a
+Sequoia -> $€qv01a
+Sequoia -> $3quo!a
+Sequoia -> 5#quo!a
+Sequoia -> $#quo!a
+Sequoia -> 5£quo!a
+Sequoia -> $£quo!a
+Sequoia -> 5€quo!a
+Sequoia -> $€quo!a
+Sequoia -> $3qvo!a
+Sequoia -> 5#qvo!a
+Sequoia -> $#qvo!a
+Sequoia -> 5£qvo!a
+Sequoia -> $£qvo!a
+Sequoia -> 5€qvo!a
+Sequoia -> $€qvo!a
+Sequoia -> $3qu0!a
+Sequoia -> 5#qu0!a
+Sequoia -> $#qu0!a
+Sequoia -> 5£qu0!a
+Sequoia -> $£qu0!a
+Sequoia -> 5€qu0!a
+Sequoia -> $€qu0!a
+Sequoia -> $3qv0!a
+Sequoia -> 5#qv0!a
+Sequoia -> $#qv0!a
+Sequoia -> 5£qv0!a
+Sequoia -> $£qv0!a
+Sequoia -> 5€qv0!a
+Sequoia -> $€qv0!a
+Sequoia -> $3quo14
+Sequoia -> 5#quo14
+Sequoia -> $#quo14
+Sequoia -> 5£quo14
+Sequoia -> $£quo14
+Sequoia -> 5€quo14
+Sequoia -> $€quo14
+Sequoia -> $3qvo14
+Sequoia -> 5#qvo14
+Sequoia -> $#qvo14
+Sequoia -> 5£qvo14
+Sequoia -> $£qvo14
+Sequoia -> 5€qvo14
+Sequoia -> $€qvo14
+Sequoia -> $3qu014
+Sequoia -> 5#qu014
+Sequoia -> $#qu014
+Sequoia -> 5£qu014
+Sequoia -> $£qu014
+Sequoia -> 5€qu014
+Sequoia -> $€qu014
+Sequoia -> $3qv014
+Sequoia -> 5#qv014
+Sequoia -> $#qv014
+Sequoia -> 5£qv014
+Sequoia -> $£qv014
+Sequoia -> 5€qv014
+Sequoia -> $€qv014
+Sequoia -> $3quo!4
+Sequoia -> 5#quo!4
+Sequoia -> $#quo!4
+Sequoia -> 5£quo!4
+Sequoia -> $£quo!4
+Sequoia -> 5€quo!4
+Sequoia -> $€quo!4
+Sequoia -> $3qvo!4
+Sequoia -> 5#qvo!4
+Sequoia -> $#qvo!4
+Sequoia -> 5£qvo!4
+Sequoia -> $£qvo!4
+Sequoia -> 5€qvo!4
+Sequoia -> $€qvo!4
+Sequoia -> $3qu0!4
+Sequoia -> 5#qu0!4
+Sequoia -> $#qu0!4
+Sequoia -> 5£qu0!4
+Sequoia -> $£qu0!4
+Sequoia -> 5€qu0!4
+Sequoia -> $€qu0!4
+Sequoia -> $3qv0!4
+Sequoia -> 5#qv0!4
+Sequoia -> $#qv0!4
+Sequoia -> 5£qv0!4
+Sequoia -> $£qv0!4
+Sequoia -> 5€qv0!4
+Sequoia -> $€qv0!4
+Sequoia -> $3quo1@
+Sequoia -> 5#quo1@
+Sequoia -> $#quo1@
+Sequoia -> 5£quo1@
+Sequoia -> $£quo1@
+Sequoia -> 5€quo1@
+Sequoia -> $€quo1@
+Sequoia -> $3qvo1@
+Sequoia -> 5#qvo1@
+Sequoia -> $#qvo1@
+Sequoia -> 5£qvo1@
+Sequoia -> $£qvo1@
+Sequoia -> 5€qvo1@
+Sequoia -> $€qvo1@
+Sequoia -> $3qu01@
+Sequoia -> 5#qu01@
+Sequoia -> $#qu01@
+Sequoia -> 5£qu01@
+Sequoia -> $£qu01@
+Sequoia -> 5€qu01@
+Sequoia -> $€qu01@
+Sequoia -> $3qv01@
+Sequoia -> 5#qv01@
+Sequoia -> $#qv01@
+Sequoia -> 5£qv01@
+Sequoia -> $£qv01@
+Sequoia -> 5€qv01@
+Sequoia -> $€qv01@
+Sequoia -> $3quo!@
+Sequoia -> 5#quo!@
+Sequoia -> $#quo!@
+Sequoia -> 5£quo!@
+Sequoia -> $£quo!@
+Sequoia -> 5€quo!@
+Sequoia -> $€quo!@
+Sequoia -> $3qvo!@
+Sequoia -> 5#qvo!@
+Sequoia -> $#qvo!@
+Sequoia -> 5£qvo!@
+Sequoia -> $£qvo!@
+Sequoia -> 5€qvo!@
+Sequoia -> $€qvo!@
+Sequoia -> $3qu0!@
+Sequoia -> 5#qu0!@
+Sequoia -> $#qu0!@
+Sequoia -> 5£qu0!@
+Sequoia -> $£qu0!@
+Sequoia -> 5€qu0!@
+Sequoia -> $€qu0!@
+Sequoia -> $3qv0!@
+Sequoia -> 5#qv0!@
+Sequoia -> $#qv0!@
+Sequoia -> 5£qv0!@
+Sequoia -> $£qv0!@
+Sequoia -> 5€qv0!@
+Sequoia -> $€qv0!@
+```
